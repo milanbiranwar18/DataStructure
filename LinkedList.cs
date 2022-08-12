76049fc40c0641ed218bee440352b48022971fbe
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-12 20:03:35 +0530

    Program for inserting the data into a LinkedList

--- LinkedList.cs
+++ LinkedList.cs
@@ -25,5 +25,20 @@ namespace DataStructure
             }
             Console.WriteLine("{0} inserted into the linked list", node.data);
         }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; 
+            }
+        }
     }
 }
