46367dee6aa02fcea8588a74c4fbe49ab61d368c
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-12 18:39:06 +0530

    Greeting Message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+namespace DataStructure
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Wellcome to DataStructure Problem");    
+
+
+        }
+    }
+}
