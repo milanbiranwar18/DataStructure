30abfb7dfe7e036e8a7e4df355fde8d76e96b528
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-12 21:48:22 +0530

    Adding program for pushing data in stack

--- Program.cs
+++ Program.cs
@@ -10,20 +10,18 @@ namespace DataStructure
         {
             Console.WriteLine("Wellcome to DataStructure Problem");
 
-            LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
-            list.Display();
-            Console.WriteLine(" ");
-            Console.WriteLine("---------------------");
+            //LinkedList list = new LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //list.Display();
+            //Console.WriteLine(" ");
+            //Console.WriteLine("---------------------");
 
             //list.InsertAtParticularPosition(2, 30);
             //list.Display();
             //Console.WriteLine(" ");
             //Console.WriteLine("30 Added between 56 and 70");
-
-
 
             //list.RemoveFirstNode();
             //list.Display();
@@ -31,18 +29,25 @@ namespace DataStructure
             //Console.WriteLine(" First element deleted ");
             //Console.WriteLine("---------------------");
 
-
             //list.RemoveLastNode();
             //list.Display();
             //Console.WriteLine(" ");
             //Console.WriteLine(" Last element deleted ");
             //Console.WriteLine("---------------------");
 
-           
-            list.Search(30);
-           list.Display();
-           
-            
+            // list.Search(30);
+            //list.Display();
+
+            Stack stack = new Stack();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+            stack.Display();
+
+
+
+
+
         }
     }
 }
